754dc4bcf99ae2b62ae9f80cb8c097fefc1c14cb
Fabian Gonzalez <fabian.glz497@gmail.com>
2025-03-10 09:22:14 -0300

    arreglo de venta al contado

--- src/components/ventas/Ventas.jsx
+++ src/components/ventas/Ventas.jsx
@@ -6,7 +6,7 @@ import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 import './Ventas.css';
 
-
+// Configuración de cuotas
 const configuracionCuotas = [
   { cuotas: 1, interes: 0 }, // Nueva opción de 1 pago sin interés
   { cuotas: 2, interes: 15 },
@@ -19,6 +19,7 @@ const configuracionCuotas = [
   { cuotas: 24, interes: 180 }
 ];
 
+// Datos de choferes
 const choferes = [
   { nombre: 'Vanesa Ferreira', patente: 'AD417CW', telefono: '11-3800-2078' },
   { nombre: 'Carolina Ferreira', patente: 'AD754DG', telefono: '11-2222-2222' },
@@ -34,8 +35,8 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
   const [cargarPrimerCuota, setCargarPrimerCuota] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredClientes, setFilteredClientes] = useState([]);
-  const [entrega, setEntrega] = useState('sucursal'); // Nueva opción para la entrega
-  const [selectedChofer, setSelectedChofer] = useState(choferes[0]); // Chofer seleccionado por defecto
+  const [entrega, setEntrega] = useState('sucursal');
+  const [selectedChofer, setSelectedChofer] = useState(null);
   const location = useLocation();
   const navigate = useNavigate();
   const sucursal = location.state?.sucursal || 'Andes 4034';
@@ -45,11 +46,15 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
 
   useEffect(() => {
     const fetchClientes = async () => {
-      const clienteCollection = collection(db, 'clientes');
-      const clienteSnapshot = await getDocs(clienteCollection);
-      const clienteList = clienteSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-      setClientes(clienteList);
-      setFilteredClientes(clienteList);
+      try {
+        const clienteCollection = collection(db, 'clientes');
+        const clienteSnapshot = await getDocs(clienteCollection);
+        const clienteList = clienteSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setClientes(clienteList);
+        setFilteredClientes(clienteList);
+      } catch (error) {
+        console.error('Error al obtener los clientes:', error);
+      }
     };
 
     fetchClientes();
@@ -66,20 +71,30 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
 
   const calcularCuotas = () => {
     if (isNaN(subtotal) || subtotal <= 0) {
-      setCuotas([{ mensaje: "Por favor, ingrese un monto válido." }]);
+      setCuotas([{ mensaje: 'Por favor, ingrese un monto válido.' }]);
       return;
     }
 
     const cuotasFiltradas = configuracionCuotas.filter(opcion => {
-      if (opcion.cuotas === 1) return true; // Siempre incluye la opción de 1 cuota
-      if (subtotal < 30000) return opcion.cuotas <= 2;
-      if (subtotal >= 30000 && subtotal < 80000) return opcion.cuotas <= 3;
-      if (subtotal >= 80000 && subtotal < 150000) return opcion.cuotas <= 6;
-      if (subtotal >= 150000 && subtotal < 250000) return opcion.cuotas <= 9;
-      if (subtotal >= 250000 && subtotal < 350000) return opcion.cuotas <= 12;
-      if (subtotal >= 350000 && subtotal < 500000) return opcion.cuotas <= 18;
-      return true;
-    });    
+      switch (true) {
+        case opcion.cuotas === 1:
+          return true; // Siempre incluye la opción de 1 cuota
+        case subtotal < 30000:
+          return opcion.cuotas <= 2;
+        case subtotal < 80000:
+          return opcion.cuotas <= 3;
+        case subtotal < 150000:
+          return opcion.cuotas <= 6;
+        case subtotal < 250000:
+          return opcion.cuotas <= 9;
+        case subtotal < 350000:
+          return opcion.cuotas <= 12;
+        case subtotal < 500000:
+          return opcion.cuotas <= 18;
+        default:
+          return true;
+      }
+    });
 
     const resultados = cuotasFiltradas.map(opcion => {
       const { cuotas, interes } = opcion;
@@ -101,7 +116,9 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
   };
 
   useEffect(() => {
-    calcularCuotas();
+    if (subtotal > 0) {
+      calcularCuotas();
+    }
   }, [subtotal, cuotasSeleccionadas]);
 
   const handleClienteChange = (e) => {
@@ -113,7 +130,7 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
   };
 
   const handleCheckboxChange = (e) => {
-    setCargarPrimerCuota(e.target.value === "sí");
+    setCargarPrimerCuota(e.target.value === 'sí');
   };
 
   const handleEntregaChange = (e) => {
@@ -130,17 +147,14 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
     setSelectedChofer(chofer);
   };
 
-
-
-
-
   const generatePDF = (venta, clienteInfo, vendedor) => {
     const doc = new jsPDF('p', 'pt', 'a4'); // A4 size
 
+    // Reemplazar las URL del logo por una variable genérica o manejada externamente
+    const logo = 'LOGO_PATH'; // Reemplazar con la ruta local o base64 del logo
+
     // Agregar el logo en la parte superior
-    const logoUrl = 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/categorias%2Flogo.png?alt=media&token=c589846d-04eb-478f-b6a6-4d91c1c8a8aa'; // URL del logo
-    // Asegúrate de usar las dimensiones correctas para que el logo no se deforme.
-    doc.addImage(logoUrl, 'JPEG', 250, 20, 80, 80); // Coordenadas X e Y, Ancho y Alto
+    doc.addImage(logo, 'JPEG', 250, 20, 80, 80); // Coordenadas X e Y, Ancho y Alto
 
     // Agregar el logo como marca de agua en el centro
     doc.setGState(new doc.GState({ opacity: 0.05 })); // Ajustar opacidad al 5%
@@ -148,7 +162,7 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
     const imgHeight = 400;
     const centerX = (doc.internal.pageSize.getWidth() - imgWidth) / 2;
     const centerY = (doc.internal.pageSize.getHeight() - imgHeight) / 2;
-    doc.addImage(logoUrl, 'JPEG', centerX, centerY, imgWidth, imgHeight, '', 'FAST'); // Ajusta la posición y tamaño según sea necesario
+    doc.addImage(logo, 'JPEG', centerX, centerY, imgWidth, imgHeight, '', 'FAST'); // Ajusta la posición y tamaño según sea necesario
     doc.setGState(new doc.GState({ opacity: 1 })); // Restaurar opacidad al 100%
 
     // Título y detalles en el centro
@@ -203,7 +217,6 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
       headStyles: { fillColor: [22, 160, 133] }
     });
 
-
     // Guardar el PDF como archivo y descargar automáticamente
     doc.save('Factura.pdf');
 
@@ -213,79 +226,86 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
     }, 1000); // Ajusta el tiempo si es necesario
   };
 
-
-
-
-
-
   const handleRealizarVenta = async () => {
-    if (!selectedCliente || !cuotasSeleccionadas || (entrega === 'domicilio' && !selectedChofer)) {
-      alert('Por favor, selecciona un cliente, las cuotas y el chofer para entrega a domicilio.');
-      return;
-    }
-
-    const cuotaSeleccionada = configuracionCuotas.find(c => c.cuotas === cuotasSeleccionadas);
-    const interes = cuotaSeleccionada?.interes || 0;
-    const totalCredito = subtotal * (1 + interes / 100);
-    const valorCuotaCalculado = Math.round(totalCredito / cuotasSeleccionadas / 1000) * 1000;
-
-    const ventasCollection = collection(db, 'ventas');
-    const venta = {
-      clienteId: selectedCliente,
-      sucursal: sucursal,
-      productos: carrito,
-      cuotas: cuotasSeleccionadas,
-      fecha: new Date(),
-      totalCredito: totalCredito,
-      valorCuota: valorCuotaCalculado,
-      pagos: [],
-      vendedor: currentUser.username,
-      entrega: entrega, // Añadir información de entrega
-      chofer: entrega === 'domicilio' ? selectedChofer : null // Añadir información del chofer si aplica
-    };
-
-    const ventaRef = await addDoc(ventasCollection, venta);
-
-    for (const producto of carrito) {
-      if (producto.categoriaId) {
-        const productoRef = doc(db, `categorias/${producto.categoriaId}/productos`, producto.id);
-        if (sucursal === 'Andes 4034') {
-          await updateDoc(productoRef, {
-            cantidadDisponibleAndes4034: producto.cantidadDisponibleAndes4034 - producto.cantidad,
-          });
-        } else if (sucursal === 'Andes 4320') {
-          await updateDoc(productoRef, {
-            cantidadDisponibleAndes4320: producto.cantidadDisponibleAndes4320 - producto.cantidad,
-          });
-        }
-      } else {
-        console.error(`Producto ${producto.id} no tiene categoriaId`);
+    try {
+      // Validación inicial
+      if (!selectedCliente || !cuotasSeleccionadas || (entrega === 'domicilio' && !selectedChofer)) {
+        alert('Por favor, selecciona un cliente, las cuotas y el chofer para entrega a domicilio.');
+        return;
       }
+
+      // Cálculo del crédito y cuota
+      const cuotaSeleccionada = configuracionCuotas.find(c => c.cuotas === cuotasSeleccionadas);
+      const interes = cuotaSeleccionada?.interes || 0;
+      const totalCredito = subtotal * (1 + interes / 100);
+      const valorCuotaCalculado = Math.round(totalCredito / cuotasSeleccionadas / 1000) * 1000;
+
+      // Creación del objeto de venta
+      const ventasCollection = collection(db, 'ventas');
+      const venta = {
+        clienteId: selectedCliente,
+        sucursal: sucursal,
+        productos: carrito,
+        cuotas: cuotasSeleccionadas,
+        fecha: new Date(),
+        totalCredito: totalCredito,
+        valorCuota: valorCuotaCalculado,
+        pagos: [],
+        vendedor: currentUser.username,
+        entrega: entrega, // Añadir información de entrega
+        chofer: entrega === 'domicilio' ? selectedChofer : null // Añadir información del chofer si aplica
+      };
+
+      // Guardar la venta en Firebase
+      const ventaRef = await addDoc(ventasCollection, venta);
+
+      // Actualización de inventario para cada producto en el carrito
+      for (const producto of carrito) {
+        if (producto.categoriaId) {
+          const productoRef = doc(db, `categorias/${producto.categoriaId}/productos`, producto.id);
+          const fieldToUpdate =
+            sucursal === 'Andes 4034'
+              ? { cantidadDisponibleAndes4034: producto.cantidadDisponibleAndes4034 - producto.cantidad }
+              : { cantidadDisponibleAndes4320: producto.cantidadDisponibleAndes4320 - producto.cantidad };
+
+          await updateDoc(productoRef, fieldToUpdate);
+        } else {
+          console.error(`Producto ${producto.id} no tiene categoriaId`);
+        }
+      }
+
+      // Obtener información del cliente
+      const clienteRef = doc(db, 'clientes', selectedCliente);
+      const clienteDoc = await getDoc(clienteRef);
+      const clienteInfo = clienteDoc.data();
+
+      // Generar el PDF
+      generatePDF(venta, clienteInfo, currentUser.username);
+
+      // Registrar el pago inicial si corresponde
+      if (cargarPrimerCuota) {
+        const fechaPago = new Date().toISOString().split('T')[0];
+        const montoPago = valorCuotaCalculado;
+        const pagosIniciales = [{ fecha: fechaPago, monto: montoPago, usuario: currentUser.username }];
+        await updateDoc(ventaRef, { pagos: pagosIniciales });
+      }
+
+      // Finalizar la venta
+      setVentaRealizada(true);
+      onClearCart();
+      alert('Venta realizada con éxito');
+    } catch (error) {
+      console.error('Error al realizar la venta:', error);
+      alert('Ocurrió un error al realizar la venta. Por favor, inténtalo nuevamente.');
     }
-
-    const clienteRef = doc(db, 'clientes', selectedCliente);
-    const clienteDoc = await getDoc(clienteRef);
-    const clienteInfo = clienteDoc.data();
-
-    generatePDF(venta, clienteInfo, currentUser.username);
-
-    if (cargarPrimerCuota) {
-      const fechaPago = new Date().toISOString().split('T')[0];
-      const montoPago = valorCuotaCalculado;
-      const pagosIniciales = [{ fecha: fechaPago, monto: montoPago, usuario: currentUser.username }];
-      await updateDoc(ventaRef, { pagos: pagosIniciales });
-    }
-
-    setVentaRealizada(true);
-    onClearCart();
-    alert('Venta realizada con éxito');
   };
-
 
   return (
     <div className="ventas">
       <h2>Realizar Venta</h2>
       {ventaRealizada && <div className="alert alert-success">¡Venta realizada con éxito!</div>}
+
+      {/* Selección de cliente */}
       <div className="form-group">
         <label htmlFor="cliente">Seleccionar Cliente:</label>
         <input
@@ -296,62 +316,134 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
           onChange={handleSearchChange}
           className="form-control"
         />
-        <select id="cliente" className="form-control" value={selectedCliente} onChange={handleClienteChange}>
+        <select
+          id="cliente"
+          className="form-control"
+          value={selectedCliente}
+          onChange={handleClienteChange}
+        >
           <option value="">-- Selecciona un Cliente --</option>
           {filteredClientes.map((cliente) => (
-            <option key={cliente.id} value={cliente.id}>{cliente.nombreCompleto}</option>
+            <option key={cliente.id} value={cliente.id}>
+              {cliente.nombreCompleto}
+            </option>
           ))}
         </select>
       </div>
+
+      {/* Selección de cuotas */}
       <div className="form-group">
         <label htmlFor="cuotasSeleccionadas">Seleccionar Cantidad de Cuotas:</label>
-        <select id="cuotasSeleccionadas" className="form-control" value={cuotasSeleccionadas} onChange={handleCuotasSeleccionadasChange}>
+        <select
+          id="cuotasSeleccionadas"
+          className="form-control"
+          value={cuotasSeleccionadas}
+          onChange={handleCuotasSeleccionadasChange}
+        >
+          <option value="">-- Selecciona las Cuotas --</option>
           {cuotas.map((opcion) => (
-            <option key={opcion.cuotas} value={opcion.cuotas}>{opcion.cuotas} cuotas de ${opcion.montoCuota} por mes</option>
+            <option key={opcion.cuotas} value={opcion.cuotas}>
+              {opcion.cuotas} cuotas de ${opcion.montoCuota} por mes
+            </option>
           ))}
         </select>
       </div>
+
+      {/* Cargar primera cuota */}
       <div className="form-group">
         <label htmlFor="cargarPrimerCuota">Cargar Primer Cuota:</label>
-        <select id="cargarPrimerCuota" className="form-control" value={cargarPrimerCuota ? "sí" : "no"} onChange={handleCheckboxChange}>
+        <select
+          id="cargarPrimerCuota"
+          className="form-control"
+          value={cargarPrimerCuota ? "sí" : "no"}
+          onChange={handleCheckboxChange}
+        >
           <option value="sí">Sí</option>
           <option value="no">No</option>
         </select>
       </div>
+
+      {/* Tipo de entrega */}
       <div className="form-group">
         <label htmlFor="entrega">Seleccionar Tipo de Entrega:</label>
-        <select id="entrega" className="form-control" value={entrega} onChange={handleEntregaChange}>
+        <select
+          id="entrega"
+          className="form-control"
+          value={entrega}
+          onChange={handleEntregaChange}
+        >
           <option value="sucursal">Retira en Sucursal</option>
           <option value="domicilio">Envío a Domicilio</option>
         </select>
       </div>
-      {entrega === 'domicilio' && (
+
+      {/* Selección de chofer (visible solo para envío a domicilio) */}
+      {entrega === "domicilio" && (
         <div className="form-group">
           <label htmlFor="chofer">Seleccionar Chofer:</label>
-          <select id="chofer" className="form-control" value={selectedChofer.nombre} onChange={handleChoferChange}>
+          <select
+            id="chofer"
+            className="form-control"
+            value={selectedChofer ? selectedChofer.nombre : ""}
+            onChange={handleChoferChange}
+          >
+            <option value="">-- Selecciona un Chofer --</option>
             {choferes.map((chofer) => (
-              <option key={chofer.nombre} value={chofer.nombre}>{chofer.nombre}</option>
+              <option key={chofer.nombre} value={chofer.nombre}>
+                {chofer.nombre}
+              </option>
             ))}
           </select>
         </div>
       )}
+
+      {/* Productos en el carrito */}
       <h3>Productos en Carrito</h3>
       <div className="card-container">
-        {carrito.map(producto => (
+        {carrito.map((producto) => (
           <div className="card" key={producto.id}>
-            <img src={producto.imagenUrl} alt={producto.nombre} className="card-img-top" />
+            <img
+              src={producto.imagenUrl}
+              alt={producto.nombre}
+              className="card-img-top"
+            />
             <div className="card-body">
               <h5 className="card-title">{producto.nombre}</h5>
-              <p className="card-text">Precio: ${producto.precio.toLocaleString('es-AR')}</p>
+              <p className="card-text">
+                Precio: ${producto.precio.toLocaleString("es-AR")}
+              </p>
               <p className="card-text">Cantidad: {producto.cantidad}</p>
-              <p className="card-text">Subtotal: ${(producto.precio * producto.cantidad).toLocaleString('es-AR')}</p>
-              <p className="card-text">Total Crédito: ${(subtotal * (1 + configuracionCuotas.find(c => c.cuotas === cuotasSeleccionadas).interes / 100)).toLocaleString('es-AR')}</p>
-              <p className="card-text">Valor de Cada Cuota: ${valorCuota.toLocaleString('es-AR')}</p>
+              <p className="card-text">
+                Subtotal: $
+                {(producto.precio * producto.cantidad).toLocaleString("es-AR")}
+              </p>
+              <p className="card-text">
+                Total Crédito: $
+                {(
+                  subtotal *
+                  (1 +
+                    configuracionCuotas.find(
+                      (c) => c.cuotas === cuotasSeleccionadas
+                    ).interes /
+                    100)
+                ).toLocaleString("es-AR")}
+              </p>
+              <p className="card-text">
+                Valor de Cada Cuota: ${valorCuota.toLocaleString("es-AR")}
+              </p>
             </div>
           </div>
         ))}
       </div>
-      <button className="btn btn-primary mt-4" onClick={handleRealizarVenta}>Realizar Venta</button>
+
+      {/* Botón de realizar venta */}
+      <button
+        className="btn btn-primary mt-4"
+        onClick={handleRealizarVenta}
+        disabled={!selectedCliente || cuotasSeleccionadas === ""}
+      >
+        Realizar Venta
+      </button>
     </div>
   );
 };

4fb6be0d759553b6dddeb3c8614390f5cff00a0d
Fabian Gonzalez <fabian.glz497@gmail.com>
2025-03-20 18:04:11 -0300

    agregado de vender para otro usuario solo jefes y encargados

--- src/components/ventas/Ventas.jsx
+++ src/components/ventas/Ventas.jsx
@@ -6,6 +6,21 @@ import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 import './Ventas.css';
 
+const usuariosDB = [
+  { username: 'Gfabian395', password: 'Gfabian395', role: 'jefe', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/vendedores%2F9.png?alt=media&token=992ee040-ed59-4b53-9013-115ee7c9fce7' },
+  { username: 'VaneDavis', password: '554972', role: 'jefe', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/vendedores%2F8.png?alt=media&token=aff23347-93dc-4737-bf1f-25f0430f34fa' },
+  { username: 'RoFlrtin', password: 'jose1946', role: 'vendedor', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/vendedores%2F6.png?alt=media&token=4b570b8c-4926-4520-bb00-69e19db6560b' },
+  { username: 'FrancoGF', password: 'Grupof2025', role: 'vendedor', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/vendedores%2F2.png?alt=media&token=38f9c73b-1442-4025-b729-615395077651' },
+  { username: 'Carmen Galarza', password: 'Gordis2024', role: 'vendedor', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/vendedores%2F5.png?alt=media&token=9530608a-7cc2-4807-bd6f-d2ce55c29c0a' },
+  { username: 'Carol B', password: 'Tokyoghoul', role: 'vendedor', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/vendedores%2F11.png?alt=media&token=b83cafcc-a9bb-4ae0-9609-2e8f65c95d10' },
+  { username: 'TamaraAbigail', password: 'Tamara07', role: 'vendedor', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/vendedores%2F3.png?alt=media&token=6a2d2262-604a-41c3-baab-051b0cd2e32a' },
+  { username: 'Yuli182', password: '244962', role: 'encargado', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/vendedores%2F1.png?alt=media&token=53e5fde2-f246-47d4-b329-436d866ac66c' },
+  { username: 'Gustavito02', password: '36520975', role: 'vendedor', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ferreirahogar-376dd.firebasestorage.app/o/vendedores%2F10.png?alt=media&token=44148120-0d0c-41ee-99aa-f4dfc4e50f7e' },
+  { username: 'Elias G', password: 'Elemento', role: 'vendedor', imageUrl: 'path/to/EliasG.jpg' },
+  { username: 'Micaela G', password: 'Galarza24', role: 'vendedor', imageUrl: 'path/to/MicaelaG.jpg' },
+  { username: 'prueba', password: 'prueba', role: 'vendedor', imageUrl: 'path/to/prueba.jpg' },
+];
+
 // Configuración de cuotas
 const configuracionCuotas = [
   { cuotas: 1, interes: 0 }, // Nueva opción de 1 pago sin interés
@@ -43,6 +58,14 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
   const subtotal = location.state?.subtotal || 0;
   const [productos, setProductos] = useState(location.state?.productos || []);
   const [valorCuota, setValorCuota] = useState(0);
+  // Estado para habilitar la opción de "Venta de otro usuario"
+  const [ventaDeOtro, setVentaDeOtro] = useState(false);
+
+  // Estado para guardar el vendedor seleccionado
+  const [selectedVendedor, setSelectedVendedor] = useState('');
+
+  // Estado para cargar la lista de usuarios (solo si el rol es jefe o encargado)
+  const [usuarios, setUsuarios] = useState(usuariosDB);
 
   useEffect(() => {
     const fetchClientes = async () => {
@@ -171,6 +194,10 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
       }
       console.log('Sucursal inicializada:', sucursal);
 
+      // Determinar el vendedor dinámicamente
+      const vendedor = ventaDeOtro && selectedVendedor ? selectedVendedor : currentUser.username;
+      console.log('Venta realizada por:', vendedor);
+
       // Cálculo de crédito y cuota
       const cuotaSeleccionada = configuracionCuotas.find(c => c.cuotas === cuotasSeleccionadas);
       const interes = cuotaSeleccionada?.interes || 0;
@@ -188,7 +215,7 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
         totalCredito,
         valorCuota: valorCuotaCalculado,
         pagos: [],
-        vendedor: currentUser.username,
+        vendedor, // Usar el vendedor determinado dinámicamente
         entrega: entrega,
         chofer: entrega === 'domicilio' ? selectedChofer : null,
       };
@@ -248,7 +275,7 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
       if (cargarPrimerCuota) {
         const ahora = new Date();
         // Formatear la fecha a YYYY-MM-DD
-        const fechaPago = ahora.toISOString().split('T')[0]; // Extrae solo la parte de la fecha
+        const fechaPago = ahora.toISOString().split('T')[0];
         const montoPago = valorCuotaCalculado;
         const pagosIniciales = [
           { fecha: fechaPago, monto: montoPago, usuario: currentUser.username },
@@ -257,7 +284,6 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
 
         console.log('Pago inicial registrado:', pagosIniciales);
       }
-
 
       // Finalizar la venta
       setVentaRealizada(true);
@@ -272,6 +298,68 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
     }
   };
 
+  // Cargar la lista de usuarios si el rol es jefe o encargado
+  /*  useEffect(() => {
+     if (['jefe', 'encargado'].includes(currentUser.role)) {
+       const fetchUsuarios = async () => {
+         try {
+           console.log('[DEBUG] Intentando obtener la colección usuarios desde Firebase...');
+           const usuariosCollection = collection(db, 'usuarios'); // Verifica la referencia
+           const usuariosSnapshot = await getDocs(usuariosCollection);
+   
+           console.log('[DEBUG] Snapshot de usuarios obtenido:', usuariosSnapshot);
+   
+           if (!usuariosSnapshot.empty) {
+             const usuariosList = usuariosSnapshot.docs.map((doc) => {
+               console.log('[DEBUG] Documento de usuario:', doc.data()); // Log de cada documento
+               return { id: doc.id, ...doc.data() };
+             });
+   
+             setUsuarios(usuariosList);
+             console.log('[DEBUG] Lista de usuarios procesada:', usuariosList);
+           } else {
+             console.warn('[DEBUG] La colección usuarios está vacía.');
+             setUsuarios([]); // Previene errores con arrays vacíos
+           }
+         } catch (error) {
+           console.error('[ERROR] Ocurrió un problema al obtener los usuarios desde Firebase:', error);
+         }
+       };
+   
+       fetchUsuarios();
+     }
+   }, [currentUser]); */
+
+  {
+    ['jefe', 'encargado'].includes(currentUser.role) && (
+      <div className="form-group">
+        <label>
+          <input
+            type="checkbox"
+            checked={ventaDeOtro}
+            onChange={(e) => setVentaDeOtro(e.target.checked)}
+            className="form-check-input"
+          />
+          Venta de otro
+        </label>
+
+        {ventaDeOtro && (
+          <select
+            value={selectedVendedor}
+            onChange={(e) => setSelectedVendedor(e.target.value)}
+            className="form-control mt-2"
+          >
+            <option value="">-- Selecciona un Vendedor --</option>
+            {usuarios.map((usuario) => (
+              <option key={usuario.username} value={usuario.username}>
+                {usuario.nombreCompleto || usuario.username}
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
+    )
+  }
 
 
   return (
@@ -304,6 +392,37 @@ const Ventas = ({ carrito, onClearCart, currentUser }) => {
           ))}
         </select>
       </div>
+
+      {/* Venta de otro usuario (solo para jefe o encargado) */}
+      {['jefe', 'encargado'].includes(currentUser.role) && (
+        <div className="form-group">
+          <label>
+            <input
+              type="checkbox"
+              checked={ventaDeOtro}
+              onChange={(e) => setVentaDeOtro(e.target.checked)}
+              className="form-check-input small-checkbox"
+              style={{ width: '20px', height: '20px', padding: '0' }}
+            />
+            Venta de otro
+          </label>
+
+          {ventaDeOtro && (
+            <select
+              value={selectedVendedor}
+              onChange={(e) => setSelectedVendedor(e.target.value)}
+              className="form-control mt-2"
+            >
+              <option value="">-- Selecciona un Vendedor --</option>
+              {usuarios.map((usuario) => (
+                <option key={usuario.id || usuario.username} value={usuario.username}>
+                  {usuario.nombreCompleto || usuario.username}
+                </option>
+              ))}
+            </select>
+          )}
+        </div>
+      )}
 
       {/* Selección de cuotas */}
       <div className="form-group">

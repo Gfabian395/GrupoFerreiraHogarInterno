50c6f8f5454e4da35a2e3be0b89dc0d9422dbbbc
Fabian Gonzalez <fabian.glz497@gmail.com>
2025-03-25 09:39:53 -0300

    arreglo de .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,19 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+// Configuración de Firebase
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.APPID
+  appId: import.meta.env.VITE_APPID
 };
 
+// Inicialización de Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
-/* 
-HASTA ACA FUNCIONA PERFECTO ESTE ES EL ORIGINAL
-*/
